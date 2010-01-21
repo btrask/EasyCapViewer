bb27f0d70d35a2635ed83a44fec426495a80aee3
btrask <bentrask@comcast.net>
2010-01-21 12:05:45 -0600

    Add resource file to the component (still incomplete).

---
+++ ECVComponent.r
@@ -0,0 +1,44 @@
+#include <Carbon/Carbon.r>
+#include <QuickTime/QuickTime.r>
+
+resource 'thng' (256)
+{
+	'vdig', // Type
+	0, // SubType
+	0, // Manufacturer
+	0, // use componentHasMultiplePlatforms
+	0,
+	0,
+	0,
+	'STR ', // Name Type
+	128, // Name ID
+	'STR ', // Info Type
+	129, // Info ID
+	0, // Icon Type
+	0, // Icon ID
+	0, // Version
+	componentHasMultiplePlatforms | myComponentRegistrationFlags, // Registration Flags
+	0, // Resource ID of Icon Family
+	{
+		0,
+		'dlle', // Entry point found by symbol name 'dlle' resource
+		256, // ID of 'dlle' resource
+#if defined(ppc_YES)
+		platformPowerPCNativeEntryPoint,
+#elif defined(i386_YES)
+		platformIA32NativeEntryPoint,
+#endif
+	};
+};
+resource 'STR ' (128)
+{
+	"ECVComponent"
+};
+resource 'STR ' (129)
+{
+	"EasyCapViewer QuickTime Video Digitizer Component"
+};
+resource 'dlle' (256)
+{
+	"ECVComponentDispatch"
+};
