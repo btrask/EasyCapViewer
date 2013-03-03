61a5dae73132f1e9d2b680269721f2b42a631615
Ben Trask <bentrask@comcast.net>
2013-03-03 18:19:04 -0500

    Revert changes to -ECV_textureName. Add assertions instead.

--- ECVAppKitAdditions.h
+++ ECVAppKitAdditions.h
@@ -26,7 +26,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 @end
 
-@interface NSImageRep(ECVAppKitAdditions)
+@interface NSBitmapImageRep(ECVAppKitAdditions)
 
 - (GLuint)ECV_textureName;
 
